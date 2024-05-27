751783e00db8dd9bfecda63a05d8ad0d970ea36b
Michael Pfammatter <pfammatter@gmail.com>
2024-05-27 09:39:46 -0400

    Switch to KDE Plasma 6

--- hosts/nixvm/default.nix
+++ hosts/nixvm/default.nix
@@ -58,14 +58,14 @@
   services.xserver.enable = true;
 
   # Enable the KDE Plasma Desktop Environment.
-  services.xserver.displayManager.sddm.enable = true;
-  services.xserver.desktopManager.plasma5.enable = true;
+  services.displayManager.sddm.wayland.enable = true;
+  services.desktopManager.plasma6.enable = true;
 
   # Configure keymap in X11
-  services.xserver = {
-    layout = "us";
-    xkbVariant = "";
-  };
+  # services.xserver = {
+  #   layout = "us";
+  #   xkbVariant = "";
+  # };
 
   # Enable CUPS to print documents.
   services.printing.enable = true;
